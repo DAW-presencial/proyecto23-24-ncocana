330142e52425380de9896d7488d91b121b6c259a
ncocana <ncocana@cifpfbmoll.eu>
2024-01-07 02:22:58 +0100

    feat(Controllers, Models, database): create table Collections

---
+++ app/Models/Collection.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Collection extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'name',
+        'description',
+    ];
+
+    public function User(): BelongsTo {
+        return $this->BelongsTo(User::class);
+    }
+}
