12ce527ba2d5a675527d52ad1058eb7bd6080868
ncocana <ncocana@cifpfbmoll.eu>
2024-02-23 20:28:43 +0100

    feat(Controllers, routes, tests): generate access token when login through api

---
+++ tests/Feature/Auth/AccessTokenTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Sanctum\PersonalAccessToken;
+use Tests\TestCase;
+
+class AccessTokenTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Creating and authenticating a user
+        $this->user = User::factory()->create([
+            'email' => 'example@example.com'
+        ]);
+    }
+
+    /** @test */
+    public function can_issue_access_token(): void
+    {
+        $this->withoutJsonApiDocumentFormatting();
+
+        $data = [
+            'email' => $this->user->email,
+            'password' => 'password',
+            'device_name' => 'My device',
+        ];
+
+        $response = $this->postJson(route('api.v1.login'), $data);
+
+        $token = $response->json('plain-text-token');
+
+        $dbToken = PersonalAccessToken::findToken($token);
+
+        $this->assertTrue($dbToken->tokenable->is($this->user));
+    }
+
+    /** @test */
+    public function password_must_be_valid(): void
+    {
+        $this->withoutJsonApiDocumentFormatting();
+
+        $data = [
+            'email' => $this->user->email,
+            'password' => 'incorrect',
+            'device_name' => 'My device',
+        ];
+
+        $response = $this->postJson(route('api.v1.login'), $data);
+
+        $response->assertStatus(422);
+        $response->assertJsonApiValidationErrors('password');
+    }
+}
