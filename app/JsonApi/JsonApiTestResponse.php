618e0ed0fe735f002d341908222b52ca0c541b63
ncocana <ncocana@cifpfbmoll.eu>
2024-02-23 20:16:04 +0100

    feat(app, tests): add json:api especification-related files

---
+++ app/JsonApi/JsonApiTestResponse.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\JsonApi;
+
+use Closure;
+use Illuminate\Support\Str;
+use Illuminate\Testing\TestResponse;
+use PHPUnit\Framework\Assert as PHPUnit;
+use PHPUnit\Framework\ExpectationFailedException;
+
+class JsonApiTestResponse
+{
+    public function assertJsonApiValidationErrors(): Closure
+    {
+        return function($attribute) {
+            /** @var TestResponse $this */
+            $pointer = Str::of($attribute)->startsWith('data')
+                ? "/".str_replace('.', '/', $attribute)
+                : "/data/attributes/{$attribute}";
+
+            try {
+                $this->assertJsonFragment([
+                    'source' => ['pointer' => $pointer]
+                ]);
+            } catch (ExpectationFailedException $e) {
+                $this->tryAlternativePointer($attribute);
+            }
+
+            try {
+                $this->assertJsonStructure([
+                    'errors' => [
+                        ['title', 'detail', 'source' => ['pointer']]
+                    ]
+                ]);
+            } catch (ExpectationFailedException $e) {
+                PHPUnit::fail("Failed to find a valid JSON:API error response"
+                    . PHP_EOL.PHP_EOL .
+                    $e->getMessage());
+            }
+
+            $this->assertHeader(
+                'content-type', 'application/vnd.api+json'
+            )->assertStatus(422);
+        };
+    }
+
+    protected function tryAlternativePointer(): Closure
+    {
+        return function($attribute) {
+            /** @var TestResponse $this */
+            $pointers = [
+                "/{$attribute}",
+                "/data/attributes/{$attribute}"
+            ];
+
+            $found = false;
+
+            foreach ($pointers as $pointer) {
+                try {
+                    $this->assertJsonFragment([
+                        'source' => ['pointer' => $pointer]
+                    ]);
+                    $found = true;
+                    // If found, break the loop
+                    break;
+                } catch (ExpectationFailedException $e) {
+                    // If not found, continue to the next pointer
+                    continue;
+                }
+            }
+
+            // If not found, return fail message
+            if (!$found) {
+                PHPUnit::fail("Failed to find a JSON:API validation error for key: '{$attribute}'"
+                    . PHP_EOL . PHP_EOL .
+                    $e->getMessage());
+            }
+        };
+    }
+}
---
+++ tests/MakesJsonApiRequests.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests;
+use Illuminate\Support\Str;
+use Illuminate\Testing\TestResponse;
+
+trait MakesJsonApiRequests
+{
+    protected bool $formatJsonApiDocument = true;
+
+    public function withoutJsonApiDocumentFormatting() {
+        $this->formatJsonApiDocument = false;
+    }
+
+    protected function getFormattedData($uri, array $data): array
+    {
+        $path = parse_url($uri)['path'];
+        $type = (string) Str::of($path)->after('api/v1/')->before('/');
+        $id = (string) Str::of($path)->after($type)->replace('/', '');
+
+        return [
+            'data' => array_filter([
+                'type' => $type,
+                'id' => $id,
+                'attributes' => $data,
+            ])
+        ];
+    }
+
+    public function json($method, $uri, array $data = [], array $headers = [], $options = 0): TestResponse
+    {
+        $headers['accept'] = 'application/vnd.api+json';
+
+        if ($this->formatJsonApiDocument) {
+            $formattedData = $this->getFormattedData($uri, $data);
+        }
+        
+        return parent::json($method, $uri, $formattedData ?? $data, $headers);
+    }
+
+    public function postJson($uri, array $data = [], array $headers = [], $options = 0): TestResponse
+    {
+        $headers['content-type'] = 'application/vnd.api+json';
+
+        return parent::postJson($uri, $data, $headers);
+    }
+
+    public function patchJson($uri, array $data = [], array $headers = [], $options = 0): TestResponse
+    {
+        $headers['content-type'] = 'application/vnd.api+json';
+
+        return parent::patchJson($uri, $data, $headers);
+    }
+}
