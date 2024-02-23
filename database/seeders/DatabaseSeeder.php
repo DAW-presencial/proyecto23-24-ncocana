b2af6e3f4c5123562d29ea5268d4cb752b243ff3
ncocana <ncocana@cifpfbmoll.eu>
2024-02-23 19:19:55 +0100

    style(seeders): eliminate unused imports

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,13 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\User;
-use App\Models\Book;
-use App\Models\Movie;
-use App\Models\Series;
-use App\Models\Collection;
-use App\Models\Bookmark;
-// use App\Models\BookmarkCollection;
 
 class DatabaseSeeder extends Seeder
 {
