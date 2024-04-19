3cd25a23e0498160b25fe8def777d06693f70ff0
Pau <desarrollo03@w34.es>
2024-04-19 15:59:02 +0200

    feat(resources):     Card.vue     SecondaryButton modified     Markers     SearchAdvanced modified     Welcome modified

---
+++ resources/js/Components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class='border border-gray-300 mt-4 p-4 min-h-36 m-5 rounded-lg'>
+        <!-- BUTTONS -->
+        <div class="flex gap-2 justify-end h-6">
+            <SecundaryButton class="bg-red-700 text-white hover:bg-red-800">DELETE</SecundaryButton>
+            <SecundaryButton class="bg-green-700 text-white hover:bg-green-800">ADD
+            </SecundaryButton>
+        </div>
+        <div>
+            <slot>
+
+            </slot>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import SecundaryButton from '@/Components/SecondaryButton.vue'
+
+</script>
