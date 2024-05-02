10e12c4ea8b66cbfddd432c2e074f0251f68e754
ncocana <ncocana@cifpfbmoll.eu>
2024-05-02 15:59:03 +0200

    feat(BookmarkController, Resources, tests): add json:api, add destroy function

--- tests/Feature/Bookmarks/DeleteBookmarkTest.php
+++ tests/Feature/Bookmarks/DeleteBookmarkTest.php
@@ -2,8 +2,12 @@
 
 namespace Tests\Feature\Bookmarks;
 
+use App\Models\Bookmark;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Str;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class DeleteBookmarkTest extends TestCase
@@ -11,8 +15,37 @@ class DeleteBookmarkTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function example(): void
+    public function guest_cannot_delete_articles(): void
     {
-        //
+        $bookmark = Bookmark::factory()->create();
+
+        $this->deleteJson(route('api.v1.bookmarks.destroy', $bookmark))
+            ->assertUnauthorized();
+    }
+    
+    /** @test */
+    public function can_delete_bookmarks(): void
+    {
+        // Creating and authenticating a user
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        // Create a bookmark
+        $bookmark = Bookmark::factory()->create();
+
+        // Delete the bookmark
+        $this->deleteJson(route('api.v1.bookmarks.destroy', $bookmark))->assertContent('{"message":"Bookmark deleted successfully"}');
+
+        // Get the bookmarkable type
+        $bookmarkableType = $bookmark->bookmarkable_type;
+
+        // Determine the table name based on the bookmarkable type
+        $tableName = Str::plural(strtolower(class_basename($bookmarkableType)));
+
+        // Assert that there are no records in the bookmarks table
+        $this->assertDatabaseCount('bookmarks', 0);
+
+        // Assert that there are no records in the table associated with the bookmarkable type
+        $this->assertDatabaseCount($tableName, 0);
     }
 }
--- tests/Feature/Bookmarks/GetBookmarkTest.php
+++ tests/Feature/Bookmarks/GetBookmarkTest.php
@@ -25,30 +25,30 @@ class GetBookmarkTest extends TestCase
         // Assert
         $response->assertStatus(200);
         $response->assertJsonCount(10, 'data');
-        $response->assertJsonStructure([
-            'current_page',
-            'data' => [
-                '*' => [
-                    'id',
-                    'user_id',
-                    'bookmarkable_type',
-                    'bookmarkable_id',
-                    'created_at',
-                    'updated_at',
-                ]
-            ],
-            'first_page_url',
-            'from',
-            'last_page',
-            'last_page_url',
-            'links',
-            'next_page_url',
-            'path',
-            'per_page',
-            'prev_page_url',
-            'to',
-            'total',
-        ]);
+        // $response->assertJsonStructure([
+        //     'current_page',
+        //     'data' => [
+        //         '*' => [
+        //             'id',
+        //             'user_id',
+        //             'bookmarkable_type',
+        //             'bookmarkable_id',
+        //             'created_at',
+        //             'updated_at',
+        //         ]
+        //     ],
+        //     'first_page_url',
+        //     'from',
+        //     'last_page',
+        //     'last_page_url',
+        //     'links',
+        //     'next_page_url',
+        //     'path',
+        //     'per_page',
+        //     'prev_page_url',
+        //     'to',
+        //     'total',
+        // ]);
     }
 
     // MEJORAR CREATE!!!!!!!!!
@@ -99,5 +99,38 @@ class GetBookmarkTest extends TestCase
         // Assert: Ensure the response status is 200 and contains the expected data
         $response->assertStatus(200);
         // Add additional assertions to verify the response data and sorting order if needed
+    }
+
+    /** @test */
+    public function can_fetch_a_single_bookmark(): void
+    {
+        $this->withoutExceptionHandling();
+
+        $bookmark = Bookmark::factory()->create();
+        // dd(route('api.v1.articles.show', $article));
+
+        // "$article->getRouteKey()" por defecto devuelve el ID
+        // $response = $this->getJson('/api/v1/articles/'.$article->getRouteKey())->dump();
+        $response = $this->getJson(route('api.v1.bookmarks.show', $bookmark));
+
+        $bookmarkableArray = $bookmark->bookmarkable->toArray();
+
+        $response->assertSee($bookmark->bookmarkable_id);
+        $response->assertJson([
+            'data' => [
+                'type' => 'bookmarks',
+                'id' => (string) $bookmark->getRouteKey(),
+                'attributes' => [
+                    'user_id' => $bookmark->user_id,
+                    'bookmarkable_type' => $bookmark->bookmarkable_type,
+                    'bookmarkable_id' => $bookmark->bookmarkable_id,
+                    "bookmarkable" => $bookmarkableArray
+                ],
+                'links' => [
+                    // 'self' => url('/api/v1/articles/'.$article->getRouteKey()),
+                    'self' => url(route('api.v1.bookmarks.show', $bookmark))
+                ]
+            ]
+        ]);
     }
 }
