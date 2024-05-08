025dfae275b8bcaa29735613e7a6aea10277796e
Agustin Tuduri Marin <atudurimarin@cifpfbmoll.eu>
2024-05-08 19:17:38 +0200

    feat(CollectionController, CollectionRequest, CollectionUpdate, CollectionResource, CollectionCollection, routes/api) Creación del CollectionController sus requests, resources y las rutas

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -2,9 +2,69 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\collection\CollectionRequest;
+use App\Http\Requests\collection\CollectionUpdate;
+use App\Http\Resources\collection\CollectionResource;
+use App\Models\Collection;
 use Illuminate\Http\Request;
 
 class CollectionController extends Controller
 {
-    //
+  
+    public function __construct()  //Aplica el Sanctum a los métodos store, update y delete
+    {
+        $this->middleware('auth:sanctum')
+        ->only([
+            'store',
+            'update',
+            'destroy'
+        ]);
+        
+    }
+
+    public function index()
+    {
+        $collection = Collection::query()     // Se usan mixins para extender builder y aplicar parámetros en la búsqueda
+            ->allowedSorts(['name', 'description'])
+            ->allowedFilters(['name', 'description'])
+            ->jsonPaginate();
+        
+        return CollectionResource::collection($collection);
+        //Se utiliza un resource para la adhesión a la especificación ApiJson de la respuesta
+    }
+
+    public function store(CollectionRequest $request) // Se utiliza un form request para la validación
+    {
+        $collection= Collection::create([
+            'name' => $request->name,
+            'description' => $request->descrption,
+        ]);
+
+        collectionResource::make($collection);
+    }
+
+    public function show(Collection $collection)
+    {
+        return CollectionResource::make($collection);
+    }
+
+    public function update(CollectionUpdate $request, Collection $collection) { 
+        //Se utiliza un formRequest especial para la validación que no tenga los campos title y director requeridos
+        $collection->fill([
+            'actors' => $request->input('actors', $collection->actors),
+            'num_seasons' => $request->input('num_seasons', $collection->num_seasons),
+        ])->save();
+        // Con Fill() y save() no hace falta meter todos los atributos en la petición sólo los que modifiquemos
+        // Con el segundo parámetro de input() nos aseguramos que si no pasamos un atributo coja los del libro por defecto
+
+        collectionResource::make($collection);
+    }
+
+    public function destroy(Collection $collection)
+    {
+        $collection->delete();
+        return response()->json([
+            "succes" =>"La colección ".$collection->id." ha sido borrada con éxito"
+        ]);
+    }
 }
