f7f237a7e277b29e3848679a8a93f859dc7142cb
ncocana <ncocana@cifpfbmoll.eu>
2024-05-17 17:43:45 +0200

    feat(BookmarkSeeder, CollectionSeeder): generate data with tags for testing

--- database/seeders/CollectionSeeder.php
+++ database/seeders/CollectionSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Bookmark;
 use App\Models\Collection;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,11 +15,40 @@ class CollectionSeeder extends Seeder
      */
     public function run(): void
     {
+        // Define your tag pool
+        $tags = ['fluff', 'angst', 'hurt/comfort', 'marvel', 'anime'];
+
+        // Create collections for a random user
         Collection::factory()
-            ->has(Bookmark::factory()->count(1)->state(function (array $attributes, Collection $collection) {
-                return ['user_id' => $collection->user_id];
-            }))
+            ->has(Bookmark::factory()->count(1))
             ->count(2)
             ->create();
+
+        // Retrieve the admin user by email
+        $user = User::where('email', 'admin@mybookmarks.com')->first();
+
+        // Create collections for admin user
+        if ($user) {
+            // Create collections for a admin user
+            $collections = Collection::factory()
+                ->has(Bookmark::factory()->count(1))
+                ->count(2)
+                ->create(['user_id' => $user->id]);
+
+            // Attach tags randomly to each collection
+            foreach ($collections as $collection) {
+                // Choose a random number of tags to attach (between 1 and the total number of tags in the pool)
+                $numTags = rand(1, count($tags));
+
+                // Shuffle the tags array to randomize the selection
+                shuffle($tags);
+
+                // Select the first $numTags from the shuffled array
+                $selectedTags = array_slice($tags, 0, $numTags);
+
+                // Attach the selected tags to the collection
+                $collection->attachTags($selectedTags);
+            }
+        }
     }
 }
