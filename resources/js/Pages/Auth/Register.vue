b0fa77fc29198a4540bdea9d1d20407d4930c0c1
“jtur17” <jtur@cifpbmoll.eu>
2024-05-22 16:52:04 +0200

    fix(resources): arreglo de token en el login

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -5,6 +5,8 @@ import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
 import { Head, Link, useForm } from '@inertiajs/vue3';
+import axios from 'axios'; // Asegúrate de importar axios
+import { onMounted } from 'vue';
 
 const form = useForm({
     name: '',
@@ -13,87 +15,74 @@ const form = useForm({
     password_confirmation: '',
 });
 
-const submit = () => {
+const submit = async () => {
     form.post(route('register'), {
+        onSuccess: () => login(),
         onFinish: () => form.reset('password', 'password_confirmation'),
     });
 };
+
+const login = async () => {
+    let params = {
+        "email": form.email, // Usa form.email
+        "password": form.password // Usa form.password
+    }
+    console.log(JSON.stringify(params));
+
+    await axios.post('/login', JSON.stringify(params))
+        .then(response => {
+            console.log(response);
+            const resultado = response.data;
+            localStorage.setItem('token', resultado.token_type + ' ' + resultado.access_token);
+        }).catch(error => {
+            console.error("Error al iniciar sesión:", error);
+        });
+}
+
+onMounted(() => {
+    localStorage.removeItem('token');
+});
 </script>
 
 <template>
     <GuestLayout>
+
         <Head title="Register" />
-        <h2 class=" mb-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign up</h2>
+        <h2 class="mb-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign up</h2>
 
         <form @submit.prevent="submit">
-
             <div>
                 <InputLabel for="name" value="Name" />
-
-                <TextInput
-                    id="name"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.name"
-                    required
-                    autofocus
-                    autocomplete="name"
-                />
-
+                <TextInput id="name" type="text" class="mt-1 block w-full" v-model="form.name" required autofocus
+                    autocomplete="name" />
                 <InputError class="mt-2" :message="form.errors.name" />
             </div>
 
             <div class="mt-4">
                 <InputLabel for="email" value="Email" />
-
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autocomplete="username"
-                />
-
+                <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required
+                    autocomplete="username" />
                 <InputError class="mt-2" :message="form.errors.email" />
             </div>
 
             <div class="mt-4">
                 <InputLabel for="password" value="Password" />
-
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="new-password"
-                />
-
+                <TextInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required
+                    autocomplete="new-password" />
                 <InputError class="mt-2" :message="form.errors.password" />
             </div>
 
             <div class="mt-4">
                 <InputLabel for="password_confirmation" value="Confirm Password" />
-
-                <TextInput
-                    id="password_confirmation"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password_confirmation"
-                    required
-                    autocomplete="new-password"
-                />
-
+                <TextInput id="password_confirmation" type="password" class="mt-1 block w-full"
+                    v-model="form.password_confirmation" required autocomplete="new-password" />
                 <InputError class="mt-2" :message="form.errors.password_confirmation" />
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <Link
-                    :href="route('login')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    Already registered?
+                <Link :href="route('login')"
+                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                Already registered?
                 </Link>
 
                 <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
@@ -102,105 +91,4 @@ const submit = () => {
             </div>
         </form>
     </GuestLayout>
-</template> -->
-
-<!-- <script setup>
-import Checkbox from '@/Components/Checkbox.vue';
-import GuestLayout from '@/Layouts/GuestLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
-import { Head, Link, useForm } from '@inertiajs/vue3';
-
-defineProps({
-    canResetPassword: {
-        type: Boolean,
-    },
-    status: {
-        type: String,
-    },
-});
-
-const form = useForm({
-    email: '',
-    password: '',
-    remember: false,
-});
-
-const submit = () => {
-    form.post(route('login'), {
-        onFinish: () => form.reset('password'),
-    });
-};
-</script>
-
-<template>
-    <div class="flex h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <GuestLayout>
-
-            <Head title="Log in" />
-
-            <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
-        </div>
-
-            <form @submit.prevent="submit">
-                <div>
-                    <InputLabel for="email" value="Email address" />
-
-                    <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus
-                        autocomplete="username" />
-
-                    <InputError class="mt-2" :message="form.errors.email" />
-                </div>
-
-                <div class="mt-4">
-                    <InputLabel for="password" value="Password">
-
-
-                        <div class="text-sm">
-                            <Link v-if="canResetPassword" :href="route('password.request')"
-                                class="font-semibold text-indigo-600 hover:text-indigo-500">
-                            Forgot password?
-                            </Link>
-
-                            <!-- <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a> -->
-                        <!-- </div>
-                    </InputLabel>
-
-                    <TextInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required
-                        autocomplete="current-password" />
-
-                    <InputError class="mt-2" :message="form.errors.password" />
-                </div>
-
-                <div class="block mt-4">
-                    <label class="flex items-center">
-                        <Checkbox name="remember" v-model:checked="form.remember" />
-                        <span class="ms-2 text-sm text-gray-600">Remember me</span>
-                    </label>
-                </div>
-
-                <div class="flex items-center justify-end mt-4">
-
-                    <PrimaryButton class="w-100 m-4" :class="{ 'opacity-25': form.processing }"
-                        :disabled="form.processing">
-                        Sign in
-                    </PrimaryButton>
-                </div>
-            </form>
-        </GuestLayout>
-    </div>
-</template> -->
-
-
-<!-- <template>
-    <div class="box-border h-32 w-90 p-4">
-        <FormLayout></FormLayout>
-    </div>
 </template>
-
-<script setup>
-    import FormLayout from "@/Layouts/FormLayout.vue";
-</script> -->
