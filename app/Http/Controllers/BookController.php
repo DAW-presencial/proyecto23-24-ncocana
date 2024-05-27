50258c7af92853d516dcbcf967d1113e24d58f5a
ncocana <ncocana@cifpfbmoll.eu>
2024-05-27 16:45:38 +0200

    fix(Controllers, Models): sort by updated_at desc

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -90,6 +90,7 @@ class BookController extends Controller
                 'title' => $validatedData['title'] ?? $bookmark->title,
                 'synopsis' => $validatedData['synopsis'] ?? $bookmark->synopsis,
                 'notes' => $validatedData['notes'] ?? $bookmark->notes,
+                'updated_at' => now(),
             ]);
         }
 
