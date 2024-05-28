e123d8f311ec4afbf1e3ffea97e5008b9213f161
“jtur17” <jtur@cifpbmoll.eu>
2024-05-28 09:50:03 +0200

    fix(LanguageSwitcher.vue): Ahora el idioma se guarda

--- resources/js/Components/LanguageSwitcher.vue
+++ resources/js/Components/LanguageSwitcher.vue
@@ -3,13 +3,13 @@
         <div class="relative rounded- w-8 h-8" @click="toggleSubMenu">
             <button class="bg-blue-500 text-white rounded-full focus:outline-none focus:ring-2 focus:ring-black">
                 <img :src="language.img" class="w-full h-full object-cover rounded-full" alt="Flag">
-                <!-- Usando la imagen SVG -->
             </button>
             <div v-if="showSubMenu"
                 class="absolute top-full left-0 bg-white border border-gray-200 py-2 px-4 rounded shadow-lg">
                 <ul>
                     <li v-for="(locale, index) in locales" :key="index">
-                        <button @click="changeLanguage(locale)" class="block py-2 text-gray-800 hover:bg-gray-200">{{
+                        <button @click="loadLanguageImg(), changeLanguage(locale)"
+                            class="block py-2 text-gray-800 hover:bg-gray-200">{{
             locale.name }}</button>
                     </li>
                 </ul>
@@ -19,10 +19,11 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import { loadLanguageAsync, getActiveLanguage } from 'laravel-vue-i18n';
 import axios from 'axios';
-const language = ref({ name: 'en', img: '/img/eeuu.svg' });
+
+const language = ref({});
 
 const locales = [
     {
@@ -34,37 +35,46 @@ const locales = [
         img: '/img/spain.svg'
     }
 ]
-const currentLocale = ref(getActiveLanguage());
+
+const currentLocale = ref(localStorage.getItem('preferredLanguage') || getActiveLanguage());
 const showSubMenu = ref(false);
 
 const changeLanguage = async (newLocale) => {
-    await loadLanguageAsync(newLocale.name); // Cargar el nuevo idioma
-    currentLocale.value = newLocale.name; // Establecer el nuevo idioma como el actual
-    await sendLanguagePreference(newLocale); // Enviar la preferencia al servidor
+    await loadLanguageAsync(newLocale.name);
+    currentLocale.value = newLocale.name;
+    localStorage.setItem('preferredLanguage', newLocale.name); // Guardar la preferencia del idioma
+    await sendLanguagePreference(newLocale);
     language.value = newLocale;
 };
 
 const sendLanguagePreference = async (newLocale) => {
     try {
-        // Realizar una solicitud POST al servidor para cambiar el idioma
         await axios.post('/language-switch', { language: newLocale });
     } catch (error) {
         console.error('Error al cambiar el idioma:', error);
     }
 };
 
+const loadLanguageImg = () => {
+    language.value = locales.find(loc => loc.name === currentLocale.value);
+};
+
 const toggleSubMenu = () => {
     showSubMenu.value = !showSubMenu.value;
 };
+
+// Cuando se monta el componente, cargar el idioma preferido
+onMounted(() => {
+    loadLanguageImg();
+    loadLanguageAsync(currentLocale.value);
+});
 
 </script>
 
 <style scoped>
 button {
     border: none;
-    /* Eliminar el borde */
     background-color: transparent;
-    /* Hacer el fondo transparente */
     cursor: pointer;
 }
 
