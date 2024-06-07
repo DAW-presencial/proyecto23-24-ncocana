71876904a64d74f53cd6045185d91d463c98705c
“jtur17” <jtur@cifpbmoll.eu>
2024-06-07 14:32:13 +0200

    silenciar mensajes axios

--- resources/js/config/axios-config.js
+++ resources/js/config/axios-config.js
@@ -23,4 +23,15 @@ axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
 axios.defaults.headers.common['Accept'] = 'application/vnd.api+json';
 axios.defaults.headers.common['Content-Type'] = 'application/vnd.api+json';
 
+// Interceptor para manejar errores en producción
+if (process.env.NODE_ENV === 'production') {
+    axios.interceptors.response.use(
+        response => response,
+        error => {
+            // Silenciar los errores en producción
+            return Promise.reject(error);
+        }
+    );
+}
+
 export default axios;
