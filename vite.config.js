367b8386c46b7772058a0b10917de258d2d78dde
“jtur17” <jtur@cifpbmoll.eu>
2024-06-07 14:18:06 +0200

    import inecesarios

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import envCompatible from 'vite-plugin-env-compatible';
 import i18n from 'laravel-vue-i18n/vite';
-import vuetify from 'vite-plugin-vuetify'
 
 export default defineConfig({
     plugins: [
